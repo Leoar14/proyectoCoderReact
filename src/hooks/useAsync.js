8dbd046912202dd01a3068396fcc74fefa14e12d
leonardo del barrio <leo.1905cai@gmail.com>
2023-05-10 00:06:18 -0300

    cambio en hooks

--- src/hooks/useAsync.js
+++ src/hooks/useAsync.js
@@ -1,29 +1,29 @@
 import { useState, useEffect, useCallback } from 'react'
 
 export const useAsync = (asyncFunction, dependencies = []) => {
-  const [data, setData] = useState(null)
-  const [error, setError] = useState(null)
-  const [loading, setLoading] = useState(true)
+    const [data, setData] = useState(null)
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
 
-  const executeAsyncFunction = useCallback(async () => {
-    setLoading(true)
-    try {
-      const data = await asyncFunction()
-      setData(data)
-    } catch (error) {
-      setError(error)
-    } finally {
-      setLoading(false)
+    const executeAsyncFunction = useCallback(async () => {
+        setLoading(true)
+        try {
+        const data = await asyncFunction()
+        setData(data)
+        } catch (error) {
+        setError(error)
+        } finally {
+        setLoading(false)
+        }
+    }, [asyncFunction])
+
+    useEffect(() => {
+        executeAsyncFunction()
+    }, [executeAsyncFunction])
+
+    return {
+        data,
+        error,
+        loading
     }
-  }, [asyncFunction])
-
-  useEffect(() => {
-    executeAsyncFunction()
-  }, [executeAsyncFunction])
-
-  return {
-    data,
-    error,
-    loading
-  }
 }
